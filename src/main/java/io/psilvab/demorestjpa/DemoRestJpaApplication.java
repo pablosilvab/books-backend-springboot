cf8665708d1b185c4424a8d43431f6cff377d9fe
Pablo Silva <pablonicolassilvabravo@gmail.com>
2020-12-29 16:34:56 -0300

    initial commit

---
+++ src/main/java/io/psilvab/demorestjpa/DemoRestJpaApplication.java
@@ -0,0 +1,13 @@
+package io.psilvab.demorestjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoRestJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoRestJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/psilvab/demorestjpa/DemoRestJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.psilvab.demorestjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoRestJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
